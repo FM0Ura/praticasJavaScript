e0ef3d59e12e6cfa99782718ee70b22601a127e6
Felipe Angelo Zucolotto Moura <fanzm.013@gmail.com>
2023-07-06 15:27:35 -0300

    v3.0

--- validadorCPF/index.js
+++ validadorCPF/index.js
@@ -1,9 +1,60 @@
 // Código com interatividade com HTML.
 
 function validCPF() {
+  var stringCPF = document.querySelector("#cpf");
+  var cpf = stringCPF.value;
+  var res = document.querySelector("#res");
+  if (stringCPF.value.length < 11) {
+    res.innerHTML = `Um CPF deve conter 11 dígitos!`;
+  } else {
+    var multiplicador = 10;
+    var soma1 = 0;
+    /* Calculo do 1º dígito */
+    // Loop para calcular o valor da soma.
+    for (i = 0; i < 9; i++) {
+      soma1 += cpf[i] * multiplicador;
+      multiplicador--;
+    }
+    var resto1 = soma1 % 11;
+    var verif1 = 0;
 
+    // Condição para o número verificador.
+    if (resto1 == 0 || resto1 == 1) {
+      verif1 = 0;
+    } else {
+      verif1 = 11 - resto1;
+    }
+
+    /* Calculo do 2º dígito */
+    var soma2 = 0;
+    multiplicador = 10; // Re-definindo o multiplicador.
+
+    // Loop para calcular o valor da soma.
+    for (i = 1; i < 10; i++) {
+      soma2 += cpf[i] * multiplicador;
+      multiplicador--;
+    }
+
+    // Condição para o número verificador.
+    var resto2 = soma2 % 11;
+    var verif2 = 0;
+    if (resto2 == 0 || resto2 == 1) {
+      verif2 = 0;
+    } else {
+      verif2 = 11 - resto2;
+    }
+
+    // Retorno para #res.
+    res.innerHTML = `<p>A soma para o 1º número deu <strong>${soma1}</strong> e o resto da primeira divisão deu <strong>${resto1}</strong>, logo o 1º dígito verificador é <strong>${verif1}</strong>.</p> 
+    <p>A soma para o 2º número deu <strong>${soma2}</strong> e o resto da primeira divisão deu <strong>${resto2}</strong>, logo o 2º dígito verificador é <strong>${verif2}</strong>.</p>`;
+    if (verif1 == cpf[9] && verif2 == cpf[10]) {
+      res.innerHTML += `<p>Parabéns! Seu CPF é <strong>válido!</strong></p>`;
+    } else {
+      res.innerHTML += `<p>Infelizmente! Seu CPF é <strong>inválido!</strong></p>`;
+    }
+  }
 }
-
+// https://www.devmedia.com.br/validar-cpf-com-javascript/23916
 // Código apenas para o Console.
 
 function validarCPF(p, s, t, q, qui, sext, set, o, n, d1, d2) {
